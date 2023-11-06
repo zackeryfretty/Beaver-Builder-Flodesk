4113bf8042c6c0f0ccbd09ebd1704f76cf771eb6
Zackery Fretty <zackeryfretty@users.noreply.github.com>
2023-11-06 18:45:37 -0500

    Initial Function Setup & CK Class Clone & Prep

---
+++ bb-flodesk.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Beaver Builder Flodesk
+ * Plugin URI: https://zackeryfretty.com
+ * Description: Adds Flodesk as an integration option when using the Subscribe Module in Beaver Builder.
+ * Version: 1.0
+ * Author: Zackery Fretty
+ * Author URI: https://zackeryfretty.com
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Constants
+define( 'BB_FLODESK_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BB_FLODESK_URL', plugins_url( '/', __FILE__ ) );
+
+// Check for Beaver Builder
+function zf_bbflo_check_for_beaver_builder() {
+  if ( class_exists( 'FLBuilder' ) ) {
+        // If Beaver Builder is installed, do the thing.
+        function zf_bbflo_add_to_sub_module( $services ) {
+            $services['flodesk'] = array(
+                'type'  => 'autoresponder',
+                'name'  => 'Flodesk',
+                'class' => 'FLBuilderServiceFlodesk',
+                'file'  => BB_FLODESK_DIR .'/classes/class-fl-builder-service-flodesk.php',
+            );
+        return $services;
+      }
+      add_filter( 'fl_builder_subscribe_form_services', 'zf_bbflo_add_to_sub_module' );
+  }
+}
+add_action( 'init', 'zf_bbflo_check_for_beaver_builder' );
